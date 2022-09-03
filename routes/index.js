14fdd0606231777487353b8d64d5c4c562297bb1
Sandeep Pasumarthi <80049188+Sandeep-Pasumarthi@users.noreply.github.com>
2022-09-03 19:56:42 +0530

    added users controller and router

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const homeController = require("../controllers/home");
 const router = express.Router();
 
 router.get("/", homeController.home);
+router.use("/users", require("./users"));
 
 module.exports = router;
